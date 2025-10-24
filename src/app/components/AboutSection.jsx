6c49843c22af58d1684d78deeaa31e3906ca5185
lawalemma24 <lawalemma24@gmail.com>
2025-10-24 22:42:12 +0100

    set up

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -53,7 +53,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/profile-pics2.jpg" width={250} className="rounded-3xl hidden md:block" height={250} />
+        <Image src="/images/profile-pics2.jpg" width={250} className="rounded-3xl hidden md:block" height={250} alt="Profile Picture" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
